f463b2637f76cb3bceadd50a3145c06f72dfb348
Lilia Parind <lilia.parind@gmail.com>
2023-08-05 13:09:08 +0300

    Creating test for Time tracking

--- cypress/e2e/issue-time-tracking.cy.js
+++ cypress/e2e/issue-time-tracking.cy.js
@@ -1,66 +1,107 @@
-describe('Issue comments creating, editing and deleting', () => {
-    beforeEach(() => {
-        cy.visit('/');
-        cy.url().should('eq', `${Cypress.env('baseUrl')}project/board`).then((url) => {
-            cy.visit(url + '/board');
-            cy.contains('This is an issue of type: Task.').click();
-        });
+describe('Issue creating and time tracking', () => {
+    
+    const description = 'Add and delete value in Time tracking'
+    const title = 'Time tracking issue'
+    
+
+beforeEach(() => {
+    cy.visit('/');
+    cy.url().should('eq', `${Cypress.env('baseUrl')}project/board`).then((url) => {
+        cy.visit(url + '/board?modal-issue-create=true');
+
+        
+        // Create new issue 
+        cy.get('.ql-editor').type(description);
+        cy.get('input[name="title"]').type(title).should('exist');
+        cy.get('[data-testid="select:reporterId"]').should('contain', 'Lord Gaben');
+        
+
+        cy.get('button[type="submit"]').click();
+        cy.get('button[type="submit"]').should('not.exist');
+
+        cy.get('[data-testid="modal:issue-create"]').should('not.exist');
+
+        cy.contains('Issue has been successfully created.').should('be.visible');
+        cy.reload();
+        cy.contains('Issue has been successfully created.').should('not.exist');
+        
+        //Assert, that new issue is created 
+        cy.get('[data-testid="board-list:backlog"]').should('be.visible');
+        cy.get('[data-testid="list-issue"]').first().contains(title).click();
+        cy.get('[data-testid="modal:issue-details"]').should('be.visible');
     });
+  });
 
-    const getIssueDetailsModal = () => cy.get('[data-testid="modal:issue-details"]');
+  it('Should add, update and remove estimation successfully', () => {
+    cy.get('[data-testid="modal:issue-details"]').should('contain','No time logged');
+    
+    //Add estimation
+    cy.get('input[placeholder="Number"]').type('10{enter}'); 
+    cy.contains('10h estimated').should('be.visible');
+    cy.get('[data-testid="icon:close"]').first().click();
+    
+    cy.get('[data-testid="board-list:backlog"]').should('be.visible');
+    cy.contains(title).click();
+    cy.get('[data-testid="modal:issue-details"]').should('be.visible');
+     
+    //Update estimation
+    cy.get('input[placeholder="Number"]').should('have.value', '10' ).clear().type('20');
+    cy.contains('20h estimated').should('be.visible');
+    cy.get('[data-testid="icon:close"]').first().click();
 
-    it('Should add, update and remove estimation successfully', () => {
-        
-        getIssueDetailsModal()
-            .find('[data-testid="icon:stopwatch"]')
-            .click();
-        
-        cy.get('[data-testid="modal:tracking"]')
-            .find('input[placeholder="Number"]')
-            .should('have.value',4)
-            .clear();
-        
-        cy.get('[data-testid="modal:tracking"]')
-            .contains('button', 'Done')
-            .click()
-            .should('not.exist');    
-            
+    cy.get('[data-testid="board-list:backlog"]').should('be.visible');
+    cy.contains(title).click();
+    cy.get('[data-testid="modal:issue-details"]').should('be.visible');
+    cy.get('input[placeholder="Number"]').should('have.value', '20' );
+    
+    //Remove estimation
+    cy.get('input[placeholder="Number"]').clear().type('{enter}');
+    cy.contains('20h estimated').should('not.exist');
+    cy.get('[data-testid="icon:close"]').first().click();
 
-        
-        getIssueDetailsModal().within(() => {
-        
-            //Add estimation    
-        cy.get('input[placeholder="Number"]').should('have.value', 8 ).clear().type (10); 
-        cy.get('[data-testid="icon:close"]').first().click();
-        
-        });
-        cy.reload();
-        cy.contains('This is an issue of type: Task.').click();
-        getIssueDetailsModal()
-            .find('input[placeholder="Number"]')
-            .should('be.visible');
-        
-           //Update estimation
-        cy.get('input[placeholder="Number"]').should('have.value', 10 ).clear().type(20);
-        cy.get('[data-testid="icon:close"]').first().click();
-        cy.reload();
-        cy.contains('This is an issue of type: Task.').click();
-        getIssueDetailsModal()
-            .find('input[placeholder="Number"]')
-            .should('be.visible');
+    cy.get('[data-testid="board-list:backlog"]').should('be.visible');
+    cy.contains(title).click();
+    cy.get('[data-testid="modal:issue-details"]').should('be.visible');
+    cy.get('input[placeholder="Number"]').should('have.value', "").and('be.visible');
 
-            //Remove estimation
-        cy.get('input[placeholder="Number"]').should('have.value', 20 ).clear();
-        cy.get('[data-testid="icon:close"]').first().click();
-        cy.reload();
-        cy.contains('This is an issue of type: Task.').click();
-        getIssueDetailsModal()
-            .find('input[placeholder="Number"]')
-            .should('be.visible');
-            
-        //getIssueDetailsModal().contains('20h estimated').should('not.exist');
+   }); 
 
-        
+   it('Should log time and remove logged time successfully', () => {
+    cy.get('input[placeholder="Number"]').type('10');
 
-    });
+    //Open pop-up dialogue 
+    cy.get('[data-testid="icon:stopwatch"]').click();
+    cy.get('[data-testid="modal:tracking"]').should('be.visible');
+    
+    //Enter value in Time tracking: time spent and time remaining
+    cy.get('input[placeholder="Number"]').eq(1).type('2');
+    cy.get('input[placeholder="Number"]').eq(2).type('5');
+    cy.contains('button', 'Done').click().should('not.exist');
+
+    cy.get('[data-testid="modal:issue-details"]').should('be.visible'); 
+    
+    //Assert, that spent and remaining time is visible
+    cy.get('[data-testid="icon:stopwatch"]').next().should('contain', '2h logged')
+           .and('contain', '5h remaining');
+    cy.get('[data-testid="icon:stopwatch"]'). next().should('not.contain','No time logged');
+    cy.contains('10h estimated').should('not.exist');
+
+
+    cy.get('[data-testid="icon:stopwatch"]').click();
+    cy.get('[data-testid="modal:tracking"]').should('be.visible');
+    
+    // Remove value from Time tracking
+    cy.get('input[placeholder="Number"]').eq(1).clear();
+    cy.get('input[placeholder="Number"]').eq(2).clear();
+    cy.contains('button', 'Done').click().should('not.exist');
+
+    cy.get('[data-testid="modal:issue-details"]').should('be.visible'); 
+    cy.get('[data-testid="icon:stopwatch"]'). next().should('have.value',"");
+    cy.get('[data-testid="icon:stopwatch"]'). next().should('contain','No time logged');
+    cy.contains('10h estimated').should('be.visible');
+
+
+   });
 });
+
+
